59d87dcbaefeb036c49c8cd1fe8676375c625d38
manu <100472310@alumnos.uc3m.es>
2023-11-29 21:32:42 +0100

    Inicio de sesión, arreglados regex de creación de usuario

--- src/script/login.js
+++ src/script/login.js
@@ -6,6 +6,48 @@ function go_to_sign_up() {
 function go_to_log_in() {
 	document.getElementById("sign-up").style.display = "none";
 	document.getElementById("log-in").style.display = "block";
+}
+
+function welcome_message(op, name=null) {
+	if (op == 1) {
+		document.getElementById("register_form_login").style.display = "none";
+		document.getElementById("register_welcome_mssg").innerHTML = "Bienvenido/a, " + name;
+		document.getElementById("register_logged_in").style.display = "grid";
+	}
+	else if (op == 0) {
+		document.getElementById("register_form_login").style.display = "grid";
+		document.getElementById("register_logged_in").style.display = "none";
+	}
+}
+
+function close_session(){
+	sessionStorage.clear();
+	welcome_message(0, "");
+}
+
+function log_in(){
+	var name = document.getElementById('login_user_name').value;
+	var pwd = document.getElementById('login_pwd').value;
+	
+	if (check_name(name) == 0) {
+		alert("Debe introducir un nombre");
+		return -1;
+	}
+	
+	if (check_user(name) == 0) {
+		alert("No existe ese usuario");
+		return -1;
+	}
+	
+	var user = localStorage.getItem(name, pwd);
+	var index = user.indexOf(";");
+	if (user.substring(index + 1) != pwd) {
+		alert("Constraseña incorrecta");
+		return -1;
+	}
+	alert("¡Sesión iniciada correctamente!");
+	welcome_message(1, name);
+	sessionStorage.setItem(name);
 } 
 
 function sign_up() {
@@ -13,7 +55,7 @@ function sign_up() {
 	var pwd = document.getElementById('signup_pwd').value;
 	var email = document.getElementById('signup_email').value;
 	
-	if (check_name(name) == -1) {
+	if (check_name(name) == 0) {
 		alert("Nombre no válido");
 		return -1;
 	}
@@ -23,27 +65,30 @@ function sign_up() {
 		return -1;
 	}
 	
-	if (check_email(email) == -1) {
-		alert("Correo electrónico no válido")
+	if (check_email(email) == 0) {
+		alert("Correo electrónico no válido");
 		return -1;
 	}
 	
-	if (check_unicity(email) == -1) {
-		alert("Ese correo ya está registrado")
+	if (check_user(name) == -1) {
+		alert("Ese usuario ya está registrado");
 		return -1;
 	}
 	
-	localStorage.setItem(email, name + ";" + pwd)
-	alert("¡Cuenta creada con exito!")
+	localStorage.setItem(name, email + ";" + pwd);
+	sessionStorage.setItem(name, pwd);
+	alert("¡Cuenta creada con exito!");
+	welcome_message(1, name);
+	go_to_log_in()
 }
 
 function check_email(email) {
-	const regex=/[\w-\.]+@([\w-]+\.)+[\w-]{2-4}/;
+	const regex = /^[a-z0-9]+@[a-z]+\.+[a-z]{2,4}$/;
 	return regex.test(email)	
 }
 
 function check_name(name) {
-	const regex=/[\w]+/;
+	const regex=/^[\w]+$/;
 	return regex.test(name)
 }
 
@@ -51,9 +96,9 @@ function check_pwd(pwd) {
 	if (pwd.length < 6) {return -1;}
 	return 0;	
 }
-function check_unicity(key) {
+
+function check_user(key) {
 	let user = localStorage.getItem(key);
-	console.log(user)
 	if (user == null) {return 0;}
 	return -1
 }
