391a4bc5a202e532a49e34c43c90d914d8bb8a64
manu <100472310@alumnos.uc3m.es>
2023-11-29 18:23:10 +0100

    creación de usuario funcional

--- src/script/login.js
+++ src/script/login.js
@@ -7,3 +7,53 @@ function go_to_log_in() {
 	document.getElementById("sign-up").style.display = "none";
 	document.getElementById("log-in").style.display = "block";
 } 
+
+function sign_up() {
+	var name = document.getElementById('signup_user_name').value;
+	var pwd = document.getElementById('signup_pwd').value;
+	var email = document.getElementById('signup_email').value;
+	
+	if (check_name(name) == -1) {
+		alert("Nombre no válido");
+		return -1;
+	}
+	
+	if (check_pwd(pwd) == -1) {
+		alert("Contraseña no válida");
+		return -1;
+	}
+	
+	if (check_email(email) == -1) {
+		alert("Correo electrónico no válido")
+		return -1;
+	}
+	
+	if (check_unicity(email) == -1) {
+		alert("Ese correo ya está registrado")
+		return -1;
+	}
+	
+	localStorage.setItem(email, name + ";" + pwd)
+	alert("¡Cuenta creada con exito!")
+}
+
+function check_email(email) {
+	const regex=/[\w-\.]+@([\w-]+\.)+[\w-]{2-4}/;
+	return regex.test(email)	
+}
+
+function check_name(name) {
+	const regex=/[\w]+/;
+	return regex.test(name)
+}
+
+function check_pwd(pwd) {
+	if (pwd.length < 6) {return -1;}
+	return 0;	
+}
+function check_unicity(key) {
+	let user = localStorage.getItem(key);
+	console.log(user)
+	if (user == null) {return 0;}
+	return -1
+}
